09f2a0cf51fe6920a6a50598306ecede35f442d0
Ralph Viktor <ralphviktor@ninetaillabs.com>
2016-11-05 12:17:31 +0200

    feat: TopShelf integration #1 In Progress
    - Add TopShelf Configuration

---
+++ VaraniumSharp.Monolith/Configuration/TopShelfConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Topshelf.Runtime;
+using VaraniumSharp.Extensions;
+using VaraniumSharp.Monolith.Enumerations;
+
+namespace VaraniumSharp.Monolith.Configuration
+{
+    /// <summary>
+    /// Provides configuration values for setting up TopShelf hosting
+    /// </summary>
+    public class TopShelfConfiguration
+    {
+        #region Properties
+
+        /// <summary>
+        /// The description of the service that will be displayed in the service control manager
+        /// </summary>
+        public string Description => ConfigurationKeys.ServiceDescription.GetConfigurationValue<string>();
+
+        /// <summary>
+        /// The display name of the service that will be displayed in the service control manager
+        /// </summary>
+        public string DisplayName => ConfigurationKeys.ServiceDisplayName.GetConfigurationValue<string>();
+
+        /// <summary>
+        /// The name of the service that will be displayed in the service control manager
+        /// </summary>
+        public string Name => ConfigurationKeys.ServiceName.GetConfigurationValue<string>();
+
+        /// <summary>
+        /// Start mode to use when the service is installed
+        /// </summary>
+        public HostStartMode StartMode => ConfigurationKeys.ServiceStartMode.GetConfigurationValue<HostStartMode>();
+
+        #endregion
+    }
+}
--- VaraniumSharp.Monolith/Enumerations/ConfigurationKeys.cs
+++ VaraniumSharp.Monolith/Enumerations/ConfigurationKeys.cs
@@ -14,5 +14,25 @@
         /// The port on which the service is hosted
         /// </summary>
         public const string ServicePort = "service.port";
+
+        /// <summary>
+        /// The name of the service as it is registered in the service control manager
+        /// </summary>
+        public const string ServiceName = "service.name";
+
+        /// <summary>
+        /// The display name of the service in the service control manager
+        /// </summary>
+        public const string ServiceDisplayName = "service.displayname";
+
+        /// <summary>
+        /// The description of the service in the service control manager
+        /// </summary>
+        public const string ServiceDescription = "service.description";
+
+        /// <summary>
+        /// Start mode to use when the service is installed
+        /// </summary>
+        public const string ServiceStartMode = "service.startmode";
     }
 }
