48303d59ed825d72533686d516b4793b3fe4022f
Ralph Viktor <ralphviktor@ninetaillabs.com>
2016-11-19 15:12:30 +0200

    feat: Logging configuration extension. Closes #5
    - Updated VaraniumSharp.Initiator to 1.4.0
    - Added extension on AutomaticContainerRegistration to automatically setup all loggers
    - Unit test for AutomaticContainerRegistration extensions

---
+++ VaraniumSharp.Monolith.Tests/Configuration/ContainerSetupExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using Serilog;
+using System.Collections.Generic;
+using VaraniumSharp.Initiator.Configuration;
+using VaraniumSharp.Initiator.DependencyInjection;
+using VaraniumSharp.Monolith.Configuration;
+
+namespace VaraniumSharp.Monolith.Tests.Configuration
+{
+    public class ContainerSetupExtensionsTests
+    {
+        #region Public Methods
+
+        [Test]
+        public void LoggingSetupWorksCorrectly()
+        {
+            // arrange
+            var containerDummy = new Mock<ContainerSetup>();
+            var logDummy = new LogDummy();
+            containerDummy.Setup(t => t.ResolveMany<BaseLogConfiguration>())
+                .Returns(new List<BaseLogConfiguration> { logDummy });
+
+            // act
+            containerDummy.Object.SetupLogging();
+
+            // assert
+            logDummy.IsActive.Should().BeTrue();
+            logDummy.WasApplied.Should().BeTrue();
+        }
+
+        #endregion
+
+        private class LogDummy : BaseLogConfiguration
+        {
+            #region Private Methods
+
+            protected override void LogSetup(LoggerConfiguration serilogConfiguration)
+            {
+                IsActive = true;
+            }
+
+            #endregion
+        }
+    }
+}
