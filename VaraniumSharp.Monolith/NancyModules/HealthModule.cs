5941b9fe91907c8aa3eba0b08c22b23fd2ddcf08
Ralph Viktor <ralphviktor@ninetaillabs.com>
2016-11-19 15:54:53 +0200

    feat: Added default health endpoint. Closes #7
    - Added default Nancy health module
    - Unit test for module

---
+++ VaraniumSharp.Monolith/NancyModules/HealthModule.cs
@@ -0,0 +1,40 @@
+﻿using Nancy;
+using VaraniumSharp.Attributes;
+using VaraniumSharp.DependencyInjection;
+
+namespace VaraniumSharp.Monolith.NancyModules
+{
+    /// <summary>
+    /// Provide default Health endpoint
+    /// </summary>
+    [AutomaticContainerRegistration(typeof(NancyModule))]
+    // ReSharper disable once ClassWithVirtualMembersNeverInherited.Global
+    public class HealthModule : NancyModule
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Parameterless Constructor
+        /// </summary>
+        public HealthModule()
+            : base("health")
+        {
+            Get[""] = parameters => HealthResult();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Basic response for calls to health endpoint
+        /// </summary>
+        /// <returns></returns>
+        protected virtual string HealthResult()
+        {
+            return "Service is online";
+        }
+
+        #endregion
+    }
+}
