5cdbf0746116de78d75fb03b8fa0793595d250be
DeadlyEmbrace <ralphviktor@ninetaillabs.com>
2016-12-05 19:38:46 +0200

    fix: TopShelf commandline arguments. Fixes #16
    - Added extension method to pull in valid TopShelf commandline arguments
    - Updated TopShelfBootstrapper so only valid arguments will be used

---
+++ VaraniumSharp.Monolith/Extensions/HostConfiguratorExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Topshelf.HostConfigurators;
+using VaraniumSharp.Monolith.Enumerations;
+
+namespace VaraniumSharp.Monolith.Extensions
+{
+    /// <summary>
+    /// Extension methods for <see cref="HostConfigurator"/>
+    /// </summary>
+    public static class HostConfiguratorExtensions
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Apply valid TopShelf command line parameters to the TopShelf configurator
+        /// </summary>
+        /// <param name="configurator">TopShelf HostConfigurator</param>
+        /// <param name="arguments">List of command line arguments</param>
+        public static void ApplyValidCommandLine(this HostConfigurator configurator, List<string> arguments)
+        {
+            var validCommandlineParameters = new List<string>();
+
+            validCommandlineParameters.AddRange(arguments.Where(t => TopShelfCommandLineArguments.Verbs.Contains(t)));
+            validCommandlineParameters.AddRange(arguments.Where(t => TopShelfCommandLineArguments.Options.FirstOrDefault(t.StartsWith) != null));
+            validCommandlineParameters.AddRange(arguments.Where(t => TopShelfCommandLineArguments.Switches.FirstOrDefault(t.StartsWith) != null));
+
+            configurator.ApplyCommandLine(string.Join(" ", validCommandlineParameters));
+        }
+
+        #endregion
+    }
+}
